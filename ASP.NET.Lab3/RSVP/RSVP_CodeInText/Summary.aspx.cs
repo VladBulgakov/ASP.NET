daad1c74e4f8ea325c0d5bc460a1b732e0c4158a
Владислав <vladhunter01gm@gmail.com>
2022-03-07 10:06:55 +0300

    Labs 2,3 added

---
+++ ASP.NET.Lab3/RSVP/RSVP_CodeInText/Summary.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace RSVP
+{
+    public partial class Summary : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected string GetNoShowHtml()
+        {
+            StringBuilder htmlStr = new StringBuilder(); 
+            var noShowData = ResponseRepository.GetRepository().GetAllResponses().Where(r => !r.WillAttend.Value); 
+            foreach (var rsvp in noShowData)
+            {
+                htmlStr.Append(String.Format("<tr><td>{0}</td><td>{1}</td><td>{2}</td>", rsvp.Name, rsvp.Email, rsvp.Phone));
+            }
+            return htmlStr.ToString();
+        }
+    }
+}
