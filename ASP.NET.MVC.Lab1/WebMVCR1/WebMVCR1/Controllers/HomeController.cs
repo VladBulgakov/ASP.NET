e3d0dfef3819966fcd00e8165966bb912b03b76c
Владислав <vladhunter01gm@gmail.com>
2022-03-11 20:45:30 +0300

    MVC Lab1 added

---
+++ ASP.NET.MVC.Lab1/WebMVCR1/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Home
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public string Index(string str)
+        {
+            string Greeting = ModelClass.ModelHello() + ", " + str; 
+            return Greeting;
+        }
+    }
+}
