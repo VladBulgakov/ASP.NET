e737d595881ce9a9fc5f2fa63ceb72b0f448f9f3
Владислав <vladhunter01gm@gmail.com>
2022-03-16 21:15:32 +0300

    Lab 2(3) MVC added

---
+++ ASP.NET.MVC.Lab2/WebMVCR1/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private static PersonRepository db = new PersonRepository();
+
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро!" : "Добрый день!";
+            ViewData["Message"] = "Хорошего дня";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+
+        public ViewResult ShowPersonList()
+        {
+            ViewBag.PersonList = db.GetAllResponses;
+            ViewBag.PersonCount = db.NumberOfPeople;
+            return View("ListPerson");
+        }
+
+        /*public string Index(string str)
+        {
+            string Greeting = ModelClass.ModelHello() + ", " + str; 
+            return Greeting;
+        }*/
+    }
+}
---
+++ ASP.NET.MVC.Lab2/WebMVCR1/WebMVCR1/Models/PersonRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCR1.Models
+{
+    public class PersonRepository
+    {
+        private List<Person> people = new List<Person>();
+        public int NumberOfPeople
+        {
+            get
+            {
+                return people.Count();
+            }
+        }
+
+        public IEnumerable<Person> GetAllResponses 
+        { 
+            get 
+            { 
+                return people; 
+            }
+        }
+
+        public void AddResponse(Person p)
+        {
+            people.Add(p);
+        }
+    }
+}
